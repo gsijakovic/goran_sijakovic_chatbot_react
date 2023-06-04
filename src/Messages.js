170a9f6d034ce6c3634e678638c7e2f3c362cdd7
gsijakovic <gsijakovic@gmail.com>
2023-06-04 17:16:52 +0200

    dodana funkcija koja će prikazivati sve poruke koje sadržavaju ime pošiljatelja i avatara

--- src/Messages.js
+++ src/Messages.js
@@ -13,3 +13,25 @@ class Messages extends Component {
 }
 
 export default Messages;
+
+renderMessage(message) {
+  const {member, text} = message;
+  const {currentMember} = this.props;
+  const messageFromMe = member.id === currentMember.id;
+  const className = messageFromMe ?
+    "Messages-message currentMember" : "Messages-message";
+  return (
+    <li className={className}>
+      <span
+        className="avatar"
+        style={{backgroundColor: member.clientData.color}}
+      />
+      <div className="Message-content">
+        <div className="username">
+          {member.clientData.username}
+        </div>
+        <div className="text">{text}</div>
+      </div>
+    </li>
+  );
+}
